26acf5e4fad0994b0baecb5e65b5f9d4e3b1b6db
swamy6 <76860063+swamy6@users.noreply.github.com>
2021-05-21 14:11:59 +0530

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,82 @@
+class Game{
+    constructor(){
+
+    }
+    getState() {
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value", function (data) {
+            gameState = data.val();
+        })
+
+    }
+
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    async start() {
+            if (gameState === 0) {
+                player = new Player();
+                var playerCountRef = await database.ref('playerCount').once("value");
+                if (playerCountRef.exists()) {
+                    playerCount = playerCountRef.val();
+                    player.getCount();
+                }
+                form = new Form()
+                form.display();
+            }
+    player1 = createSprite(200,500);
+    player1.addImage("player1",player1img);
+    
+   
+    players=[player1];
+
+        }
+    
+    play(){
+        
+                form.hide();
+
+                Player.getPlayerInfo();
+                 image(freefireimg, 0, 0, 1000, 800);
+                 var x =100;
+                 var y=200;
+                 var index =0;
+                 drawSprites();
+                 for(var plr in allPlayers){
+                    
+                    
+                     index = index+1;
+                     x = 500-allPlayers[plr].distance;
+                     y=500;
+                     
+                    
+                       
+                     if(index === player.index){
+                         
+                         fill("yellow");
+                         textSize(25);
+                         text(allPlayers[plr].name ,x-25,y+25);
+
+                         
+                     }
+                    
+                         textSize(25);
+                         fill("white");
+                         text("Player 1 :" +allPlayers.player1.score,50,50);
+                       
+                 
+                 }
+                
+                
+                 
+
+               
+                }
+        
+
+    end(){
+       console.log("Game Ended");
+    }
+}
